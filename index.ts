14dbb294c8ace4e10096a43d3ebcef70dae3dc99
ManzoorHassan <161144604+ManzoorHassan@users.noreply.github.com>
2024-04-22 09:42:24 -0700

    adding a currency convaror project

---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+
+const currency: any = {
+    USD: 1, 
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PAK: 280,
+};
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            type: "list",
+            message:"Enter From Currency",
+            choices:["USD","EUR","GBP","INR","PAK"]
+        },
+        {
+            name: "to",
+            type: "list",
+            message: "Enter To Currency",
+            choices:["USD","EUR","GBP","INR","PAK"]
+        },
+        {
+            name: "amount",
+            message: "Enter Your Amount",
+            type: "number",
+        }
+    ]
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
